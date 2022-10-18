0b6b889a110d68cb9d18af9ce8d0dfc81e399617
kpl <kuopingl+1@gmail.com>
2022-10-18 23:04:38 +0800

    add: insertion sort

---
+++ Sorting/Sorting/InsertionSort.cpp
@@ -0,0 +1,61 @@
+using namespace std;
+#include <iostream>
+#include <vector>
+
+class InsertionSort {
+    public:
+    static void sort(vector<int>& nums, bool iteration) {
+
+        if (nums.size() <= 1) return;
+        
+        if (iteration) {
+            iterationSort(nums);
+        } else {
+            recursiveSort(nums);
+        }
+
+    }
+    
+    static void iterationSort(vector<int>& nums) {
+        
+        for (int i = 1 ; i < nums.size() ; i++) {
+            for (int j = i; j > 0 && lessThan(nums[j], nums[j-1]); j--) {
+                swap(nums, j, j-1);
+            }
+        }
+    }
+
+    static void recursiveSort(vector<int>& nums, int index = 1) {
+        if (index == nums.size()) return;
+        for (int j = index; j > 0 && lessThan(nums[j], nums[j-1]); j--) {
+            swap(nums, j, j-1);
+        }
+
+        recursiveSort(nums, index+1);
+    }
+
+    static bool lessThan(int a, int b) {
+        return a < b;
+    }
+
+    static void swap(vector<int>& nums, int indexA, int indexB) {
+        int a = nums[indexA];
+        nums[indexA] = nums[indexB];
+        nums[indexB] = a;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    vector<int> nums = {5, 9, 3, 1, 2, 8, 4, 7, 6};//{9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    InsertionSort::sort(nums, false);
+
+    cout << '{';
+    for (auto i : nums) {
+        cout << i << ", ";
+    }
+    cout << "}";
+
+    return 0;
+}
