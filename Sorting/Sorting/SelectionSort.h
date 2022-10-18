ac2461c9d6438e93aecba10567b22cc335950c52
kpl <kuopingl+1@gmail.com>
2022-10-19 00:12:25 +0800

    add: recursive sort

--- Sorting/Sorting/SelectionSort.h
+++ Sorting/Sorting/SelectionSort.h
@@ -3,7 +3,16 @@ using namespace std;
 
 class SelectionSort {
     public:
-    static void sort(vector<int> &nums) {
+    static void sort(vector<int> &nums, bool iteration) {
+        if (iteration) {
+            iterationSort(nums);
+        } else {
+            recursiveSort(nums);
+        }
+    }
+
+    private:
+    static void iterationSort(vector<int>& nums) {
         for(int i = 0 ; i < nums.size() - 1 ; i++) {
             
             int min_index = i;
@@ -20,5 +29,25 @@ class SelectionSort {
                 nums[i] = min;
             }
         }
+    }
+
+    static void recursiveSort(vector<int>& nums, int index = 0) {
+        int min_index = index;
+
+        if (min_index == nums.size()) return;
+
+        for (int j = index + 1 ; j < nums.size() ; j++) {
+            if (nums[j] < nums[min_index]) {
+                min_index = j;
+            }
+        }
+        
+        if (min_index != index) {
+            int min = nums[min_index];
+            nums[min_index] = nums[index];
+            nums[index] = min;
+        }
+
+        recursiveSort(nums, ++index);
     }
 };
